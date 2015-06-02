5e50e33c797c1ea2c4fa6dd3ca5bbf1b1585a047
Damian Mee <damian.wasilewski@gmail.com>
2015-06-02 14:09:34 +0700

    link fixed

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -5,7 +5,7 @@ var p = document.getElementsByClassName('gb_ma gb_r gb_na gb_0c')[0];
 var n = p.getElementsByTagName('span')[0];
 
 var a = document.createElement('a');
-a.href = 'https://plus.google.com/me';
+a.href = 'https://plus.google.com';
 a.innerHTML = '+' + n.innerHTML;
 a.style.color = '#404040';
 
